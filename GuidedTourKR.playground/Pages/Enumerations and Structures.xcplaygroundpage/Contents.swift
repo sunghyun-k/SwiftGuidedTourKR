c4f5fdcdf57b88074c8f23d444bdbb5bf1a50c63
Sunghyun Kim <sunghyun_k@icloud.com>
2019-05-19 15:01:48 +0900

    카드 구조체의 반환 값을 변경함

--- GuidedTourKR.playground/Pages/Enumerations and Structures.xcplaygroundpage/Contents.swift
+++ GuidedTourKR.playground/Pages/Enumerations and Structures.xcplaygroundpage/Contents.swift
@@ -90,7 +90,7 @@ struct Card {
     var rank: Rank
     var suit: Suit
     func simpleDescription() -> String {
-        return "The \(rank.simpleDescription()) of \(suit.simpleDescription())"
+        return "\(suit.simpleDescription()) \(rank.simpleDescription()))"
     }
 }
 let threeOfSpades = Card(rank: .three, suit: .spades)
